d05e2535ad457a318f6ee82cd8dca2d80b538f38
rasalgh0ul <arkantos983@gmail.com>
2022-06-28 18:04:20 -0300

    'ssss'

---
+++ js/1-holamundo.js
@@ -0,0 +1,15 @@
+// Comentario de unica linea
+
+/* Comentario de 
+multiples 
+lineas */
+
+// mostrar un mensaje en la pantalla o documenta
+//javascript no es necesario escribir el ; al final de una linea pero es recomendado para que sea legible
+document.write("Hola comision 6i");
+
+//mostrar mensaje en la consola
+console.log("prueba de consola");
+
+// mostrar un mensaje en una ventana emergente
+alert("desde una ventana emergente");
