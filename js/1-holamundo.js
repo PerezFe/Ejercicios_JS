4c92a6efc8a3df4e3a7d1ac09245721bdcd01947
rasalgh0ul <arkantos983@gmail.com>
2022-06-30 17:31:13 -0300

    agregamos ejemplo de prompt

--- js/1-holamundo.js
+++ js/1-holamundo.js
@@ -6,10 +6,27 @@ lineas */
 
 // mostrar un mensaje en la pantalla o documenta
 //javascript no es necesario escribir el ; al final de una linea pero es recomendado para que sea legible
-document.write("Hola comision 6i");
+//document.write("Hola comision 6i");
 
 //mostrar mensaje en la consola
-console.log("prueba de consola");
+//console.log("prueba de consola");
 
 // mostrar un mensaje en una ventana emergente
-alert("desde una ventana emergente");
+//alert("desde una ventana emergente");
+
+//sumar 2 valores
+//document.write("resultado: " + (12 + 5))
+
+
+//alert ("un mensaje");
+
+document.write("Hello World");
+document.write("<br>"+  (3+5));
+
+//var userName = prompt("Cual es tu nombre?");
+//alert("Hola " + userName)
+
+parseInt, x = +prompt("Escribe un numero");
+parseInt, y = +prompt("Escribe otro numero");
+alert ("La suma es " + (x+y));
+
